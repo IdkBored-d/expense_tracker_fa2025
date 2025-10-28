5c747cef10fd56a024e0dcdcbda3a8b518538e26
Devin <Devinhe.dev@gmail.com>
2025-10-28 12:20:49 -0400

    Add Expense Logic Finished

--- lib/new_expense.dart
+++ lib/new_expense.dart
@@ -5,8 +5,8 @@ import 'package:intl/intl.dart';
 final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget{
-  const NewExpense({super.key});
-  
+  const NewExpense({super.key, required this.onAddExpense});
+  final void Function(Expense expense) onAddExpense;
   @override
   State<NewExpense> createState() {
     return _NewExpenseState();
@@ -29,7 +29,7 @@ class _NewExpenseState extends State<NewExpense>{
   void _submitExpenseData(){
     final enteredAmount = double.tryParse(_amountController.text);
     final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
-    if(_titleController.text.trim().isEmpty || amountIsInvalid){
+    if(_titleController.text.trim().isEmpty || amountIsInvalid || _selectedDate == null){
       showDialog(
         context: context,
          builder: (ctx) => AlertDialog(
@@ -45,6 +45,15 @@ class _NewExpenseState extends State<NewExpense>{
          );
          return;
   }
+    widget.onAddExpense(
+      Expense(
+      title: _titleController.text,
+      amount: enteredAmount,
+      date: _selectedDate!,
+      category: _selectedCategory
+      )
+    );
+    Navigator.pop(context);
   }
 
   Future<void> _presentDatePicker() async {
@@ -64,7 +73,7 @@ class _NewExpenseState extends State<NewExpense>{
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(16),
+      padding: EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
