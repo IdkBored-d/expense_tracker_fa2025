72299a79f7d2b4046dc9cdfce7a73f0adbc01e99
Devin <Devinhe.dev@gmail.com>
2025-10-30 12:27:44 -0400

    Finished making widgets dismissing

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 class Expenses extends StatefulWidget{
   const Expenses({super.key});
 
+@override
   State<Expenses> createState(){
     return _ExpensesState();
   }
@@ -26,6 +27,22 @@ class _ExpensesState extends State<Expenses>{
     });
   }
 
+  _removeExpense(Expense expense){
+    int expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Expense Deleted'),
+      duration: Duration(seconds: 5),
+      action: SnackBarAction(label: 'Undo', onPressed: (){
+        setState(() {
+          _registeredExpenses.insert(expenseIndex, expense);
+        });
+      })
+    ));
+  }
+
   final List<Expense> _registeredExpenses = [
     Expense(
       title: 'Cheeseburger',
@@ -43,6 +60,15 @@ class _ExpensesState extends State<Expenses>{
   
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(child: Text("Click the + button to add an Expense!"));
+
+    if(_registeredExpenses.isNotEmpty){
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+        );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Tracker'),
@@ -56,7 +82,7 @@ class _ExpensesState extends State<Expenses>{
       body: Column(
         children: [
           Text("Chart"), 
-        Expanded(child: ExpensesList(expenses: _registeredExpenses))
+        Expanded(child: mainContent)
         ]),
     );
   }
