52d1902852ab9be81ccf2b2718ce64c8017aa42b
Devin <Devinhe.dev@gmail.com>
2025-11-13 12:10:30 -0500

    Made expenses have check for width and swap between a row and a column

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -61,6 +61,9 @@ class _ExpensesState extends State<Expenses>{
   
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    print("Width ${MediaQuery.of(context).size.width}");
+    print("Height ${MediaQuery.of(context).size.height}");
     Widget mainContent = const Center(child: Text("Click the + button to add an Expense!"));
 
     if(_registeredExpenses.isNotEmpty){
@@ -81,12 +84,18 @@ class _ExpensesState extends State<Expenses>{
              ),
         ],
       ),
-      body: Column(
+      body: width < 600 ? Column(
         children: [
           Chart(expenses:_registeredExpenses), 
         Expanded(child: mainContent)
-        ]),
+        ],
+        )
+        : Row(
+          children: [
+          Expanded(child:Chart(expenses:_registeredExpenses)), 
+          Expanded(child: mainContent)
+          ],
+        ),
     );
   }
-  
 }
